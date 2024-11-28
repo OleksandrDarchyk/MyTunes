84716ee47bb311a40092264e40cc14b41380e7fb
JaneyFan <fanjaney9@gmail.com>
2024-11-28 20:31:58 +0100

    Add bll layer-MyTunesManager

---
+++ src/main/java/dk/easv/mytunes/bll/MyTunesManager.java
@@ -0,0 +1,20 @@
+package dk.easv.mytunes.bll;
+
+import dk.easv.mytunes.be.Song;
+import dk.easv.mytunes.dal.ISongDAO;
+import dk.easv.mytunes.dal.SongDAO;
+import dk.easv.mytunes.dal.db.SongDAODB;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MyTunesManager {
+    private final SongDAO songDAO = new SongDAO();
+
+
+    // Get all songs
+    public List<Song> getSongs() throws IOException {
+        return songDAO.getAll();
+    }
+
+}

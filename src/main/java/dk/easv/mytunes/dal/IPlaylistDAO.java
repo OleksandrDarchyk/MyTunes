85b3b43e3e04a37b086214759c2e8296659cf95d
OleksandrDarchyk <oledar01@easv365.dk>
2024-12-04 13:38:10 +0100

    1.Add methods to PlaylistDAODB -deleteSongFromPlaylist -addSongToPlaylist -getSongInPlaylist

--- src/main/java/dk/easv/mytunes/dal/IPlaylistDAO.java
+++ src/main/java/dk/easv/mytunes/dal/IPlaylistDAO.java
@@ -1,6 +1,7 @@
 package dk.easv.mytunes.dal;
 
 import dk.easv.mytunes.be.Playlist;
+import dk.easv.mytunes.be.Song;
 
 import java.util.List;
 
@@ -8,4 +9,8 @@ public interface IPlaylistDAO {
     List<Playlist> getAllPlaylists();
     void addPlaylist(Playlist playlist);
     void deletePlaylist(int playlistID);
+    void updatePlaylist(Playlist playlist);
+    List<Song> getSongInPlaylist(int playlistID);
+    void addSongToPlaylist(int playlistID, int songID);
+    void deleteSongFromPlaylist(int playlistID, int songID);
 }

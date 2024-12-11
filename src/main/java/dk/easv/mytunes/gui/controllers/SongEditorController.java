540c74bc9c0b2195378c109fac7bfa7cf307ee4e
OleksandrDarchyk <oledar01@easv365.dk>
2024-12-11 08:58:43 +0100

    new comment

--- src/main/java/dk/easv/mytunes/gui/controllers/SongEditorController.java
+++ src/main/java/dk/easv/mytunes/gui/controllers/SongEditorController.java
@@ -66,30 +66,36 @@ public class SongEditorController implements Initializable {
     }
 
     public void onChooseClick(ActionEvent actionEvent) {
+        // Open a file chooser dialog to select a song file
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Select a Song File");
+        fileChooser.setTitle("Select a Song File"); // Set the title of the file chooser window
         fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Audio Files", "*.mp3", "*.wav", "*.m4a")
+                new FileChooser.ExtensionFilter("Audio Files", "*.mp3", "*.wav", "*.m4a") // Restrict file types to common audio formats
         );
 
+        // Show the file chooser and store the selected file
         File selectedFile = fileChooser.showOpenDialog(btnChoose.getScene().getWindow());
         if (selectedFile != null) {
+            // If a file is selected, set its path in the text field
             txtFilePath.setText(selectedFile.getAbsolutePath());
 
             try {
+                // Create a Media object from the selected file
                 Media media = new Media(selectedFile.toURI().toString());
+                // Create a MediaPlayer object to interact with the media
                 MediaPlayer mediaPlayer = new MediaPlayer(media);
 
+                // When the media is ready, calculate its duration
                 mediaPlayer.setOnReady(() -> {
                     double durationInSeconds = media.getDuration().toSeconds();
                     int minutes = (int) (durationInSeconds / 60);
                     int seconds = (int) (durationInSeconds % 60);
-                    String timeString = String.format("%d:%02d", minutes, seconds); // Формат 4:23
+                    String timeString = String.format("%d:%02d", minutes, seconds);
                     txtTime.setText(timeString);
                 });
             } catch (Exception e) {
                 myTunesController.showWarningDialog("File Error", "Unable to read the selected audio file.");
-                txtTime.setText("00:00");
+                txtTime.setText("00:00");// Set a default value for the time in case of an error
             }
         }
 
@@ -107,7 +113,7 @@ public class SongEditorController implements Initializable {
             String category = comboBox.getValue();
             String filePath = txtFilePath.getText();
 
-            Time time = Time.valueOf("00:04:00");
+            Time time = Time.valueOf("00:"+txtTime.getText());
 
             Song newSong = new Song(title, artist, category, time, filePath);
 
@@ -118,6 +124,7 @@ public class SongEditorController implements Initializable {
             Stage stage = (Stage) btnSave.getScene().getWindow();
             stage.close();
         } catch (Exception e) {
+            //e.printStackTrace();
             System.out.println("Error saving song: " + e.getMessage());
         }
     }
